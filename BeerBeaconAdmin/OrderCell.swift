ddcbc51a9dc5d7cab80c4d4382de1124fc8f93b8
Bruno <brunoteixeiralc@gmail.com>
2017-05-16 00:30:13 -0300

    commit - order

---
+++ BeerBeaconAdmin/OrderCell.swift
@@ -0,0 +1,13 @@
+//
+//  OrderCell.swift
+//  BeerBeaconAdmin
+//
+//  Created by Bruno Corrêa on 15/05/17.
+//  Copyright © 2017 Br4Dev. All rights reserved.
+//
+
+import UIKit
+
+class OrderCell:UITableViewCell{
+    
+}
---
+++ MainOrderTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MainOrderTableViewController.swift
+//  BeerBeaconAdmin
+//
+//  Created by Bruno Corrêa on 15/05/17.
+//  Copyright © 2017 Br4Dev. All rights reserved.
+//
+
+import UIKit
+
+class MainOrderTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderCell", for: indexPath) as! OrderCell
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "Pronto!"
+    }
+
+}
